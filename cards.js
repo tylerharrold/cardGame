58ec1aa7df3d8dd4a744c06e7d35f0bcc460776d
tylerharrold <tylerharrold@gmail.com>
2015-12-28 23:18:11 -0500

    added hand class and button display functions

--- cards.js
+++ cards.js
@@ -1,3 +1,32 @@
+/*
+ *****************************************
+  Class definitions, utility functions, and globals
+ *****************************************
+*/
+
+// Globals
+var handsToPlay = 1; // number of hands to play (default of one hand)
+var betPerHand =  1; // bet per hand (default of one unit)
+var betCounter = 0; // variable to track times the bet button has been clicked
+const valueOfBet = .05; // default value of a bet, this is constant
+var money =  1.00; // money started with, cannot bet more than have Note: for testing this has been set to $10.00
+var totalBet = 0.0; // variable for storing total value  of current bet
+
+// refresh the total bet
+function refreshTotalBet(){
+  totalBet = valueOfBet * betPerHand * handsToPlay;
+  // NOTE: CHANGE THE HTML DISPLAY IN THE SPAN THAT DOESN'T YET EXIST
+}
+
+// Utility functions
+function canAffordBet(){
+  refreshTotalBet();
+  //if bet doesn't surpass the money, return true
+  // NOTE: because of this call, its letting the bet get higher than the  money
+  return (totalBet <= money);
+}
+
+// Function to return a proper card suit
 function returnSuitBasedOnLoopValue(val){
   switch (val){
     case 0:
@@ -10,7 +39,7 @@ function returnSuitBasedOnLoopValue(val){
       return "spades";
   }
 }
-
+// Function to return a proper card name
 function returnNumberBasedOnLoopValue(val){
   switch(val){
     case 1:
@@ -26,6 +55,7 @@ function returnNumberBasedOnLoopValue(val){
   }
 }
 
+// Card class
 function Card(val , suit){
   this.val = val;
   this.suit  = suit;
@@ -39,8 +69,9 @@ function Card(val , suit){
   };
 };
 
+// Deck class
 function Deck(){
-  // create an array of cards
+  // array of 52 cards
   this.cards =  [];
   this.populateDeck = function(){
     for(var i = 0 ; i < 4 ; i++){
@@ -57,24 +88,122 @@ function Deck(){
     }
   };
   this.shuffleDeck = function(){
-    this.cards.forEach(
-      function(card){
-        // generate random index of array to swap with
-        var randIndex = Math.floor(Math.rand() * 52);
-        // create a placeholder to store value of current index card
-        var placeholderCard = card.clone();
-        // set the values of the current index to those of the random index card
-        card.setValues(this.cards[randIndex].val , this.cards[randIndex].suit);
-        // set the values of the random index card to the temp stored values
-        this.cards[randIndex].setValues(placeholderCard.val , placeholderCard.suit);
-      }
-    );
+    for(var i = 0 ; i < this.cards.length ; i++){
+      //generate random index of array to swap with
+      var randIndex = Math.floor(Math.random() * 52);
+      //create a placeholder to store value of current index card
+      var placeholderCard = card.clone();
+      // set the values of the current index to those of the random index card
+      card.setValues(this.cards[randIndex].val , this.cards[randIndex].suit);
+      // set the values of the random index card to the temp stored values
+      this.cards[randIndex].setValues(placeholderCard.val , placeholderCard.suit);
+    }
   };
 }
 
-var deck = new Deck();
-deck.populateDeck();
+// Hand class
+function Hand(){
+  this.hand = []; // array of five card objects
+  this.handWinnings = 0; // winning for the hand (default 0)
+  this.drawHand = function(deck){
+    // save the first five cards from the shuffled deck into a hand
+    for(var i = 0 ; i < 5 ; i++){
+      this.hand[i] = deck[i].clone();
+    }
+    // remove the drawn cards from the deck
+    deck.splice(0 , 5);
+  };
+  // NEED FUNCTION TO ANALYZE HAND AND TALLY PAYOUT
+}
 
+/*
+ *****************************************
+  Button Behavior/ HTML modifications
+ *****************************************
+*/
+
+// NOTE the way this is structured, bets can get higher than the money because the check at even or lower allows one final change
+        // that puts it above
+
+// function and callback to alter handsToPlay by button click
+function alterHands(){
+  // if can afford bet, increment handsToPlay or loop back down to one, refresh total bet value
+  if(canAffordBet()){
+    switch(handsToPlay){
+      case 1:
+        handsToPlay = 2;
+        break;
+      case 2:
+        handsToPlay = 3;
+        break;
+      case 3:
+        handsToPlay  = 1;
+        break;
+    }
+    refreshTotalBet(); // bet has changed, refresh total bet
+    redisplayTotalBet();
+  }
+
+  // change the display of button, refresh total bet
+  document.querySelector("#hands-to-play").textContent = "Hands to Play: " + handsToPlay;
+};
+document.querySelector("#hands-to-play").addEventListener("click" , alterHands); // listener
+
+// function and  callback to alter betPerHand by button click
+function alterBet(){
+  // increment betPerHand or loop back down
+  if(canAffordBet()){
+    switch(betPerHand){
+      case 1:
+        betPerHand = 2;
+        break;
+      case 2:
+        betPerHand = 3;
+        break;
+      case 3:
+        betPerHand = 5;
+        break;
+      case 5:
+        betPerHand  = 10;
+        break;
+      case 10:
+        betPerHand = 1;
+        break;
+    }
+    refreshTotalBet(); // bet has changed, refresh total bet
+    redisplayTotalBet();
+  }
+  // change the display of button
+  document.querySelector("#bet-per-hand").textContent = "Bet Per Hand: " + betPerHand;
+};
+document.querySelector("#bet-per-hand").addEventListener("click" , alterBet); // listener
+
+// fucntion and callback for deal button
+// button click event listener
+document.querySelector("#deal").addEventListener("click" , dealFirstHand) // listener
+
+// function to set totalBet display
+function redisplayTotalBet(){
+  document.querySelector("#total-bet").textContent = "Total Bet: " + totalBet.toFixed(2);
+};
+
+// function to set totalMoney display
+function redisplayTotalMoney(){
+  document.querySelector("#total-money").textContent = "Total Money: " + money.toFixed(2);
+};
+
+/*
+ *****************************************
+  Game Logic
+ *****************************************
+*/
+
+// Page load setups
+refreshTotalBet();
+redisplayTotalMoney();
+redisplayTotalBet();
+
+// testing function can be deleted later
 function displayNodeNumber(){
   var list = document.querySelectorAll(".card");
 
@@ -83,4 +212,24 @@ function displayNodeNumber(){
   }
 }
 
-document.querySelector("button").addEventListener("click" , displayNodeNumber)
+
+// On page load, create 3 decks for each row of cards and the first hand
+// deck for testing can be deleted later
+var deckOne = new Deck();
+deckOne.populateDeck();
+deckOne.shuffleDeck();
+
+var handOne = new Hand();
+
+
+// function to deal out first hand
+function dealFirstHand(){
+  // draw hand and remove drawn cards from deck
+  handOne.drawHand(deckOne.cards);
+  // display the five cards on the node list of dirs
+  var cardNodeList = document.querySelectorAll(".card");
+  for(var i = 10 ; i < cardNodeList.length ; i++){
+    var content = handOne.hand[i - 10].val + " of " + handOne.hand[i - 10].suit;
+    cardNodeList[i].querySelector("p").textContent = content;
+  }
+};
